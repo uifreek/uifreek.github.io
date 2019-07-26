789d7bb9b0b91c30a06c72cefd86e9fb8118d8b7
uifreek <49269667+uifreek@users.noreply.github.com>
2019-07-26 22:41:45 +0530

    Add files via upload

---
+++ precache-manifest.8b0c95bc34e4a73b144770928753d6d9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e0bb8bcb4f3dc264e5418a21882d3c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8dc8bd3356a3d3e9a3f",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3fe109fed8df0ebf693d",
+    "url": "/static/css/main.e13ba0f2.chunk.css"
+  },
+  {
+    "revision": "d8dc8bd3356a3d3e9a3f",
+    "url": "/static/js/2.80ea88b0.chunk.js"
+  },
+  {
+    "revision": "3fe109fed8df0ebf693d",
+    "url": "/static/js/main.47be7535.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "03fa72934057f1d67e0c3e9a6f9ffcf1",
+    "url": "/static/media/client-1.03fa7293.png"
+  },
+  {
+    "revision": "75af860a5604ff306f253efbc1bbb32e",
+    "url": "/static/media/client-2.75af860a.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1e98cff3fdd410807778f5377f7b2c68",
+    "url": "/static/media/main-bg.1e98cff3.jpg"
+  },
+  {
+    "revision": "811585164ab7b7829e2c1ea74e64a6f6",
+    "url": "/static/media/s-automation.81158516.jpg"
+  },
+  {
+    "revision": "ba002193fa3af2723c060d5bb4596dfd",
+    "url": "/static/media/s-bank.ba002193.jpg"
+  },
+  {
+    "revision": "19923b81222ab3150c8a4a4768f1725e",
+    "url": "/static/media/s-devops.19923b81.jpg"
+  },
+  {
+    "revision": "a6f91002d2e55b83e120491e58680c52",
+    "url": "/static/media/s-mobileapp.a6f91002.jpg"
+  }
+]);
